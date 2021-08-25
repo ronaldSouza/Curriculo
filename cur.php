0ac66f5dd81451a3de0a5f1a969c300a863b7577
Ronald Souza <telles.s@escolar.ifrn.edu.br>
2021-08-25 07:05:43 -0300

    teste 2 - sucesso

--- cur.php
+++ cur.php
@@ -1,9 +1,6 @@
 <?php
-    
-    
-    
-    
 
+        // print_r("aaaaaaaaaa");
     $nome = $_POST['nome'];
     $idade = $_POST['idade'];
     $phone = $_POST['tel'];
@@ -14,12 +11,12 @@
     $estado = $_POST['estado'];
 
     $obj = $_POST['obj'];
-    
+        
     $curso = $_POST['curso'];
     $inst = $_POST['inst'];
     $data_conc = $_POST['data_conc'];
     $opcao = $_POST['opcao'];
-    
+        
     $emp_ent = $_POST['data_ent'];
     $emp_sai = $_POST['data_sai'];
     $cargo = $_POST['cargo'];
@@ -28,11 +25,11 @@
 
     date_default_timezone_set('America/Sao_Paulo');
 
-    
+        
     $novo_nome = "";
     $img_dir = "";
     $dir = "uploads". DIRECTORY_SEPARATOR;
-    if(isset($_FILES['foto'])) {
+    if(isset($_FILES['foto']) and $_FILES['foto']['name'] <> "") {
 
         $ext = explode(".", strtolower($_FILES['foto']['name']))[1];
         $novo_nome = date("Y.m.d-H.i.s") . ".".$ext;
@@ -40,11 +37,11 @@
         $img_dir = $dir.$novo_nome;
 
         move_uploaded_file($_FILES['foto']['tmp_name'], $img_dir);
-   }
-       
-    
+    }
+        
+        
     $conc = "";
-    
+        
     $nova_data = date("d/m/Y", strtotime($data_conc));
     if ($opcao == "concluido") {
         $conc = "Curso Concluído em ". $nova_data;
@@ -55,15 +52,14 @@
     $nova_emp_ent = date("d/m/Y", strtotime($emp_ent));
     $nova_emp_sai = date("d/m/Y", strtotime($emp_sai));
 
-    
+        
     // organizar o formato do telefone
     $phone_size = strlen($phone);
     $phone_aux = "";
 
     for ($i = 0; $i < $phone_size; $i++) {
         if ($i == 2) {
-            $phone_aux = $phone_aux. " ";
-            
+            $phone_aux = $phone_aux. " ";      
         }
             $phone_aux = $phone_aux. $phone[$i];
     }
@@ -88,10 +84,6 @@
         array_push($idiomas,"Indonésio");
     }
 
-    if ($_SERVER['REQUEST_URI'] == "/Curriculo/cur.php") {
-        header("Location: curriculo_pag.php");
-        
-        
-    }
+    include 'curriculo_pag.html';
 
 ?>
